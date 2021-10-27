e75b4fb25232afa704baf5eb1d8669b5f017b622
horsicq <horsicq@gmail.com>
2021-10-27 18:55:00 +0200

    Fix: 2021-10-27

--- xmemorymapwidget.cmake
+++ xmemorymapwidget.cmake
@@ -1,8 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# TODO
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XHexView/xhexview.cmake)
 
 set(XMEMORYMAPWIDGET_SOURCES
+    ${XFORMATS_SOURCES}
+    ${XLINEEDITHEX_SOURCES}
+    ${XHEXVIEW_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogmemorymap.ui
     ${CMAKE_CURRENT_LIST_DIR}/xmemorymapwidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogmemorymap.cpp
