022e004de00c3621813a073210ee886cc3d1f7ff
horsicq <horsicq@gmail.com>
2024-12-03 01:01:32 +0100

    Update file(s): 2024-12-03

--- xmemorymapwidget.cmake
+++ xmemorymapwidget.cmake
@@ -1,9 +1,14 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+if (NOT DEFINED XFORMATS_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+    set(XMEMORYMAPWIDGET_SOURCES ${XMEMORYMAPWIDGET_SOURCES} ${XFORMATS_SOURCES})
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
 
 set(XMEMORYMAPWIDGET_SOURCES
+    ${XMEMORYMAPWIDGET_SOURCES}
     ${XFORMATS_SOURCES}
     ${XLINEEDITHEX_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogmemorymap.cpp
